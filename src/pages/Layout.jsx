6271a1bebf234cf5cd6e057a3e37aeba56c5b996
IvanOrsh <ksironov@mail.ru>
2023-07-18 17:31:46 +0300

    chore: install tss-react

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,12 +1,34 @@
 import { Outlet } from "react-router-dom";
 
+import { makeStyles } from "tss-react/mui";
+
 import NavBar from "../components/NavBar/NavBar";
 
-const Layout = () => (
-  <>
-    <NavBar />
-    <Outlet />
-  </>
-);
+const useStyles = makeStyles()(() => ({
+  root: {
+    display: "flex",
+    height: "100%",
+  },
+  toolbar: {
+    height: "70px",
+  },
+  content: {
+    flexGrow: "1",
+    padding: "2em",
+  },
+}));
+
+const Layout = () => {
+  const { classes } = useStyles();
+  return (
+    <div className={classes.root}>
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Outlet />
+      </main>
+    </div>
+  );
+};
 
 export default Layout;
