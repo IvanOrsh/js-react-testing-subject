29111a5ed5b55c28c528246fdc8f8772c64be60d
IvanOrsh <ksironov@mail.ru>
2023-07-21 01:34:28 +0300

    feat: even more movie info on movie detail page

--- src/pages/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage.jsx
@@ -31,7 +31,16 @@ import useStyles from "./MovieDetailPage.styles";
 const MovieDetailPage = () => {
   const { id } = useParams();
   const { classes } = useStyles();
+
+  const addToFavorites = () => {};
+  const addToWatchlist = () => {};
+
+  const isMovieFavorited = false;
+  const isMovieWatchlisted = false;
+
   const { data, isFetching, error } = useGetMovieQuery(id);
+
+  console.log(data);
 
   const dispatch = useDispatch();
 
@@ -52,64 +61,180 @@ const MovieDetailPage = () => {
   }
 
   return (
-    <Grid container className={classes.containerSpaceAround}>
-      <Grid item container sm={12} lg={4}>
-        <img
-          className={classes.poster}
-          src={
-            data.poster_path
-              ? `https://image.tmdb.org/t/p/w500/${data.poster_path}`
-              : "http://via.placeholder.com/200x300"
-          }
-          alt={data?.title}
-        />
-      </Grid>
-      <Grid item container direction="column" lg={7}>
-        <Typography variant="h4" align="center" gutterBottom>
-          {data?.title} ({data?.release_date.split("-")[0]})
-        </Typography>
-        <Typography variant="h5" align="center" gutterBottom>
-          {data?.tagline}
-        </Typography>
-        <Grid item container className={classes.containerSpaceAround}>
-          <Box display="flex" justifyContent="center">
-            <Rating readOnly value={data.vote_average / 2} />
-            <Typography
-              variant="subtitle1"
-              gutterBottom
-              style={{ marginLeft: "10px " }}
-            >
-              {data?.vote_average} / 10
-            </Typography>
-          </Box>
-          <Typography variant="h6" align="center">
-            {data?.runtime}min{" "}
-            {data?.spoken_languages.length > 0
-              ? `/ ${data.spoken_languages[0].name}`
-              : ""}
+    <>
+      <Grid container className={classes.containerSpaceAround}>
+        {/* Movie Poster */}
+        <Grid item container sm={12} lg={6}>
+          <img
+            className={classes.poster}
+            src={
+              data.poster_path
+                ? `https://image.tmdb.org/t/p/w500/${data.poster_path}`
+                : "http://via.placeholder.com/200x300"
+            }
+            alt={data?.title}
+          />
+        </Grid>
+
+        {/* Movie Info */}
+        <Grid item container direction="column" lg={6}>
+          <Typography variant="h4" align="center" gutterBottom>
+            {data?.title} ({data?.release_date.split("-")[0]})
           </Typography>
-        </Grid>
-        <Grid item container className={classes.genresContainer}>
-          {data?.genres?.map((genre) => (
-            <Link
-              key={genre.name}
-              className={classes.links}
-              to="/"
-              onClick={() => dispatch(selectGenreOrCategory(genre.id))}
-            >
-              <img
-                src={genreIcons[genre.name.toLowerCase()]}
-                className={classes.genreImage}
-                height={30}
-              />
-              <Typography color="InfoText" variant="subtitle1">
-                {genre?.name}
+          <Typography
+            color="textSecondary"
+            variant="h5"
+            align="center"
+            gutterBottom
+          >
+            {data?.tagline}
+          </Typography>
+
+          {/* Rating, Runtime, Language */}
+          <Grid item container className={classes.containerSpaceAround}>
+            <Box display="flex" justifyContent="center">
+              <Rating readOnly value={data.vote_average / 2} />
+              <Typography
+                variant="subtitle1"
+                gutterBottom
+                style={{ marginLeft: "10px " }}
+              >
+                {data?.vote_average} / 10
               </Typography>
-            </Link>
-          ))}
+            </Box>
+            <Typography variant="h6" align="center">
+              {data?.runtime}min{" "}
+              {data?.spoken_languages.length > 0
+                ? `/ ${data.spoken_languages[0].name}`
+                : ""}
+            </Typography>
+          </Grid>
+
+          {/* Movie Genres: */}
+          <Grid item container className={classes.genresContainer}>
+            {data?.genres?.map((genre) => (
+              <Link
+                key={genre.name}
+                className={classes.links}
+                to="/"
+                onClick={() => dispatch(selectGenreOrCategory(genre.id))}
+              >
+                <img
+                  src={genreIcons[genre.name.toLowerCase()]}
+                  className={classes.genreImage}
+                  height={30}
+                />
+                <Typography color="textPrimary" variant="subtitle1">
+                  {genre?.name}
+                </Typography>
+              </Link>
+            ))}
+          </Grid>
+
+          {/* Movie Overview  */}
+          <Typography variant="h5" gutterBottom style={{ marginTop: "2rem" }}>
+            Overview
+          </Typography>
+          <Typography style={{ marginBlock: "1rem " }}>
+            {data?.overview}
+          </Typography>
+
+          {/* Movie Cast */}
+          <Typography variant="h5" gutterBottom>
+            Top Cast{" "}
+          </Typography>
+          <Grid item container spacing={2}>
+            {data &&
+              data.credits?.cast?.slice(0, 6).map((actor) => (
+                <Grid
+                  item
+                  container
+                  key={actor.id}
+                  xs={4}
+                  md={2}
+                  component={Link}
+                  to={`/actors/${actor.credit_id}`}
+                  style={{ textDecoration: "none" }}
+                >
+                  <img
+                    className={classes.castImage}
+                    src={
+                      actor.profile_path
+                        ? `https://image.tmdb.org/t/p/w500/${actor.profile_path}`
+                        : "http://via.placeholder.com/200x300"
+                    }
+                    alt={actor.name}
+                  />
+                  <Typography color="textPrimary">{actor?.name}</Typography>
+                  <Typography color="textSecondary">
+                    {actor?.character.split("/")[0]}
+                  </Typography>
+                </Grid>
+              ))}
+          </Grid>
         </Grid>
       </Grid>
-    </Grid>
+
+      {/* Buttons TODO: refactor */}
+      <Box className={classes.buttonsContainer}>
+        <Button
+          variant="outlined"
+          target="_blank"
+          rel="noopener noreferrer"
+          href={data?.homepage}
+          endIcon={<Language />}
+        >
+          Website
+        </Button>
+        <Button
+          variant="outlined"
+          target="_blank"
+          rel="noopener noreferrer"
+          href={`https://www.imdb.com/title/${data?.imdb_id}`}
+          endIcon={<MovieIcon />}
+        >
+          IMDB
+        </Button>
+        <Button
+          variant="outlined"
+          onClick={() => {}}
+          href="#"
+          endIcon={<Theaters />}
+        >
+          Trailer
+        </Button>
+
+        <Button
+          variant="outlined"
+          onClick={addToFavorites}
+          endIcon={isMovieFavorited ? <FavoriteBorderOutlined /> : <Favorite />}
+        >
+          {isMovieFavorited ? "Unfavorite" : "Favorite"}
+        </Button>
+        <Button
+          variant="outlined"
+          onClick={addToWatchlist}
+          endIcon={isMovieWatchlisted ? <Remove /> : <PlusOne />}
+        >
+          Watchlist
+        </Button>
+        <Button
+          variant="outlined"
+          endIcon-={<ArrowBack />}
+          sx={{ borderColor: "primary.main" }}
+        >
+          <Typography
+            component={Link}
+            to="/"
+            color="inherit"
+            variant="subtitle2"
+            style={{ textDecoration: "none" }}
+          >
+            Back
+          </Typography>
+        </Button>
+      </Box>
+    </>
   );
 };
 
--- src/pages/MovieDetailPage.styles.js
+++ src/pages/MovieDetailPage.styles.js
@@ -3,8 +3,9 @@ import { makeStyles } from "tss-react/mui";
 export default makeStyles()((theme) => ({
   containerSpaceAround: {
     display: "flex",
+    alignItems: "start",
     justifyContent: "space-around",
-    margin: "10px 0 !important",
+    margin: "px 0 !important",
     [theme.breakpoints.down("sm")]: {
       flexDirection: "column",
       flexWrap: "wrap",
@@ -12,19 +13,17 @@ export default makeStyles()((theme) => ({
   },
 
   poster: {
+    margin: "0 auto",
     borderRadius: "20px",
     boxShadow: "0.5em 1em 1em rgb(64, 64, 70)",
-    width: "80%",
+    marginBottom: "2rem",
+    maxHeight: "600px",
 
     [theme.breakpoints.down("md")]: {
-      margin: "0 auto",
       width: "50%",
-      marginBottom: "30px",
     },
     [theme.breakpoints.down("sm")]: {
-      margin: "0 auto",
       height: "50%",
-      marginBottom: "30px",
     },
   },
 
@@ -41,11 +40,30 @@ export default makeStyles()((theme) => ({
   },
 
   links: {
+    textDecoration: "none",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
     [theme.breakpoints.down("sm")]: {
       padding: "0.5rem 1rem",
+    },
+  },
+
+  castImage: {
+    width: "100%",
+    maxWidth: "7em",
+    objectFit: "cover",
+    borderRadius: "10px",
+  },
+
+  buttonsContainer: {
+    marginTop: "2rem",
+    display: "flex",
+    width: "100%",
+    wrap: "wrap",
+    justifyContent: "center",
+    [theme.breakpoints.down("md")]: {
+      flexDirection: "column",
     },
   },
 }));
