a3c5153620a9a25e73af949430a9496f8d52201c
IvanOrsh <ksironov@mail.ru>
2023-07-20 17:08:15 +0300

    feat: scaffold movie detail page

--- src/pages/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage.jsx
@@ -1,3 +1,116 @@
-const MovieDetailPage = () => <div>MovieDetailPage</div>;
+import {
+  Modal,
+  Typography,
+  Button,
+  ButtonGroup,
+  Grid,
+  Box,
+  CircularProgress,
+  useMediaQuery,
+  Rating,
+} from "@mui/material";
+import {
+  Movie as MovieIcon,
+  Theaters,
+  Language,
+  PlusOne,
+  Favorite,
+  FavoriteBorderOutlined,
+  Remove,
+  ArrowBack,
+} from "@mui/icons-material";
+import { Link, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+
+import { useGetMovieQuery } from "../services/TMDB";
+import { selectGenreOrCategory } from "../features/currentGenreOrCategory";
+import genreIcons from "../assets/genres";
+import useStyles from "./MovieDetailPage.styles";
+
+const MovieDetailPage = () => {
+  const { id } = useParams();
+  const { classes } = useStyles();
+  const { data, isFetching, error } = useGetMovieQuery(id);
+
+  const dispatch = useDispatch();
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <CircularProgress size="8rem" />
+      </Box>
+    );
+  }
+
+  if (error) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <Link to="/">Something has gone wrong - Go back</Link>
+      </Box>
+    );
+  }
+
+  return (
+    <Grid container className={classes.containerSpaceAround}>
+      <Grid item container sm={12} lg={4}>
+        <img
+          className={classes.poster}
+          src={
+            data.poster_path
+              ? `https://image.tmdb.org/t/p/w500/${data.poster_path}`
+              : "http://via.placeholder.com/200x300"
+          }
+          alt={data?.title}
+        />
+      </Grid>
+      <Grid item container direction="column" lg={7}>
+        <Typography variant="h4" align="center" gutterBottom>
+          {data?.title} ({data?.release_date.split("-")[0]})
+        </Typography>
+        <Typography variant="h5" align="center" gutterBottom>
+          {data?.tagline}
+        </Typography>
+        <Grid item container className={classes.containerSpaceAround}>
+          <Box display="flex" justifyContent="center">
+            <Rating readOnly value={data.vote_average / 2} />
+            <Typography
+              variant="subtitle1"
+              gutterBottom
+              style={{ marginLeft: "10px " }}
+            >
+              {data?.vote_average} / 10
+            </Typography>
+          </Box>
+          <Typography variant="h6" align="center">
+            {data?.runtime}min{" "}
+            {data?.spoken_languages.length > 0
+              ? `/ ${data.spoken_languages[0].name}`
+              : ""}
+          </Typography>
+        </Grid>
+        <Grid item container className={classes.genresContainer}>
+          {data?.genres?.map((genre) => (
+            <Link
+              key={genre.name}
+              className={classes.links}
+              to="/"
+              onClick={() => dispatch(selectGenreOrCategory(genre.id))}
+            >
+              <img
+                src={genreIcons[genre.name.toLowerCase()]}
+                className={classes.genreImage}
+                height={30}
+              />
+              <Typography color="InfoText" variant="subtitle1">
+                {genre?.name}
+              </Typography>
+            </Link>
+          ))}
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default MovieDetailPage;
