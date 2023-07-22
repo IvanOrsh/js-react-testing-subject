7ea630e44bf55b7a43a66e24369226f9a1a46cab
IvanOrsh <ksironov@mail.ru>
2023-07-22 20:07:43 +0300

    feat: pagination component

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as MovieList } from "./MovieList/MovieList";
 export { default as Search } from "./Search/Search";
 export { default as ExpandableText } from "./ExpandableText/ExpandableText";
 export { default as ErrorGoBack } from "./ErrorGoBack/ErrorGoBack";
+export { default as Pagination } from "./Pagination/Pagination";
