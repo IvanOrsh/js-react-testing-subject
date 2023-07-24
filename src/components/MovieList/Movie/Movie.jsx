e06df26d5ae8deac1a2eff97f18fb4c755b135ff
IvanOrsh <ksironov@mail.ru>
2023-07-25 02:15:43 +0300

    fix: change styles to card

--- src/components/MovieList/Movie/Movie.jsx
+++ src/components/MovieList/Movie/Movie.jsx
@@ -1,35 +1,50 @@
-import { Typography, Grid, Grow, Tooltip, Rating } from "@mui/material";
-import { Link } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
+import {
+  Grow,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Typography,
+} from "@mui/material";
 import useStyles from "./styles";
 
 const Movie = ({ movie, i }) => {
   const { classes } = useStyles();
+  const navigate = useNavigate();
 
   return (
-    <Grid item xs={12} sm={6} md={4} lg={3} xl={2} className={classes.movie}>
-      <Grow in key={i} timeout={(i + 1) * 250}>
-        <Link className={classes.links} to={`/movie/${movie.id}`}>
-          <img
-            alt={movie.title}
-            className={classes.image}
-            src={
+    <Grow style={{ borderRadius: "5px" }} in key={i} timeout={(i + 1) * 250}>
+      <Card onClick={() => navigate(`/movie/${movie.id}`)}>
+        <CardActionArea className={classes.root}>
+          <CardMedia
+            component="img"
+            alt={movie.original_title}
+            image={
               movie.poster_path
                 ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
                 : "http://via.placeholder.com/200x300"
             }
+            title={movie.original_title}
+            className={classes.media}
           />
-          <Typography className={classes.title} variant="h5">
-            {movie.title}
-          </Typography>
-          <Tooltip disableTouchListener title={`${movie.vote_average} / 10`}>
-            <div>
-              <Rating readOnly value={movie.vote_average / 2} precision={0.1} />
-            </div>
-          </Tooltip>
-        </Link>
-      </Grow>
-    </Grid>
+          <CardContent className={classes.infoContainer}>
+            <Typography className={classes.title} variant="h6" component="h2">
+              {movie.title}
+            </Typography>
+            <Typography variant="body2" className={classes.info}>
+              Original Language: {movie.original_language}
+            </Typography>
+            <Typography variant="body2" className={classes.info}>
+              Release Date: {movie.release_date}
+            </Typography>
+            <Typography variant="body2" className={classes.info}>
+              Vote Average: {movie.vote_average}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Grow>
   );
 };
 
--- src/components/MovieList/Movie/styles.js
+++ src/components/MovieList/Movie/styles.js
@@ -1,40 +1,54 @@
 import { makeStyles } from "tss-react/mui";
 
 export default makeStyles()((theme) => ({
-  movie: {
-    padding: "10px",
-  },
-
-  links: {
-    alignItems: "center",
-    fontWeight: "bolder",
-    textDecoration: "none",
-    [theme.breakpoints.up("xs")]: {
-      display: "flex",
-      flexDirection: "column",
-    },
-    "&:hover": {
-      cursor: "pointer",
-    },
-  },
-
-  image: {
-    borderRadius: "20px",
-    height: "300px",
+  root: {
+    position: "relative",
+    transition: "transform 0.4s",
     marginBottom: "10px",
     "&:hover": {
       transform: "scale(1.05)",
     },
+
+    [theme.breakpoints.down("sm")]: {
+      width: "250px",
+    },
+  },
+  media: {
+    height: "360px",
+  },
+
+  infoContainer: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    width: "100%",
+    background: "rgba(0, 0, 0, 0.8)",
+    color:
+      theme.palette.mode === "dark"
+        ? theme.palette.secondary
+        : theme.palette.primary.contrastText,
+    padding: theme.spacing(2),
+    transition: "bottom 0.2s",
+  },
+
+  info: {
+    color:
+      theme.palette.mode === "dark"
+        ? theme.palette.secondary.main
+        : theme.palette.common.white,
   },
 
   title: {
-    color: theme.palette.text.primary,
+    color:
+      theme.palette.mode === "light"
+        ? theme.palette.primary.light
+        : "invert(1)",
     textOverflow: "ellipsis",
     width: "230px",
     whiteSpace: "nowrap",
     overflow: "hidden",
     marginTop: "10px",
-    marginBottom: 0,
-    textAlign: "center",
+    marginBottom: "5px",
+    textAlign: "left",
   },
 }));
