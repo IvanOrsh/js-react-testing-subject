0f09b148e666f152068a855001be3391c8e151ec
IvanOrsh <ksironov@mail.ru>
2023-07-25 12:33:12 +0300

    fix: accept additional excludeFrom prop

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -3,12 +3,13 @@ import { Grid } from "@mui/material";
 import Movie from "./Movie/Movie";
 import useStyles from "./styles";
 
-const MovieList = ({ movies, numberOfMovies }) => {
+const MovieList = ({ movies, numberOfMovies, excludeFirst }) => {
   const { classes } = useStyles();
+  const startFrom = excludeFirst ? 1 : 0;
 
   return (
     <Grid container className={classes.moviesContainer}>
-      {movies?.slice(0, numberOfMovies).map((movie, i) => (
+      {movies?.slice(startFrom, numberOfMovies).map((movie, i) => (
         <Movie key={movie.id} movie={movie} i={i} />
       ))}
     </Grid>
